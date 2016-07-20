c20b47fe61929c2eab830325b5a751eed6df2c96
vivekn94 <viveknagaraju1994@gmail.com>
2016-07-20 18:34:52 +0530

    removed comments from static controller

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -4,12 +4,9 @@ class StaticPagesController < ApplicationController
   def home
 
   	    @review = current_user.reviews.build if user_signed_in?
-  	    #@comment=current_user.reviews.comments.build if user_signed_in?
   	    @reviews=current_user.reviews
   	    if params[:search]
   	    @reviewsearch = Review.search(params[:search]).order("created_at DESC")
-        # @user=User.find(params[:id])
-  	  #  @comments=@reviewsearch.comments
   	    @users=User.all
         
   end
